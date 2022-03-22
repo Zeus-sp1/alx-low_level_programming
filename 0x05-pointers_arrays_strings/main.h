992965ce2b5995646f7b2413d1b2e23161ab71e3
Zeus-sp1 <72125524+Zeus-sp1@users.noreply.github.com>
2022-03-22 06:35:49 -0700

    new

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+int _putchar(char c):
+void swap_int(int *a, int*b,)[[[C):
+int _strlen(char *s);[A[;[B[B[A[C[C[C[C[C[C[C[;[D[D[D[D[D[D[D[B[D
+void _puts(char*str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _aioi(char *s)
+;
+ls
+[A[A[B[C[A[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C):[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
